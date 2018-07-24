e18094c22509ba10257a85bbc37fa9db5ba04633
Johnny Trentin <johnnyrtrentin@gmail.com>
2018-07-23 21:42:47 -0300

    consertado welcome.html

--- app/server.js
+++ app/server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('welcome-page'));
 app.use(express.static('public'));
 
 app.get('/', function (req,res) {
-    res.sendFile(__dirname + '/login-page/login.html')
+    res.sendFile(__dirname + '/welcome-page/welcome.html')
 });
 
 app.listen(3000,function () {
-    console.log('UP NO 3000!');
+    console.log('ONLINE');
 });
